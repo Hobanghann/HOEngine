ac013bec503f30134e8cb67e692fb049d7c63f47
Hoin Lee <hobanghann@gmail.com>
2026-05-11 03:41:01 +0900

    feat: Add Main module

---
+++ Main/EngineMain.cc
@@ -0,0 +1,18 @@
+#include "EngineMain.h"
+
+#include "Core/Log/Logger.h"
+#include "Platforms/IPlatformApplication.h"
+
+namespace ho
+{
+int EngineMain(const EngineMainParam& param)
+{
+    Logger::Init();
+    
+    IPlatformApplication::GetInstance().Init(L"Editor", 1280, 720);
+
+    while (IPlatformApplication::GetInstance().ProcessPlatformMessages());
+
+    return 0;
+}
+} // namespace ho
